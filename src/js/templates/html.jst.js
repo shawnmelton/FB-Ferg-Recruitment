68984c07a7ed4f1f7c982fef93bb33252d584fb4
Shawn Melton <shawn.a.melton@gmail.com>
2013-08-09 16:14:14 -0400

    Changes pulled down from FTP

--- src/js/templates/html.jst.js
+++ src/js/templates/html.jst.js
@@ -5,19 +5,19 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<table id="events-list-table">\n\t';
+__p += '<table id="events-list-table">\r\n\t';
  _.each(events, function(event) { ;
-__p += '\n\t<tr' +
+__p += '\r\n\t<tr' +
 ((__t = ( event.class )) == null ? '' : __t) +
-'>\n\t\t<td width="40%">' +
+'>\r\n\t\t<td width="40%">' +
 ((__t = ( event.location )) == null ? '' : __t) +
-'</td>\n\t\t<td width="35%">' +
+'</td>\r\n\t\t<td width="35%">' +
 ((__t = ( event.title )) == null ? '' : __t) +
-'</td>\n\t\t<td width="25%">' +
+'</td>\r\n\t\t<td width="25%">' +
 ((__t = ( event.date )) == null ? '' : __t) +
-'</td>\n\t</tr>\n\t';
+'</td>\r\n\t</tr>\r\n\t';
  }); ;
-__p += '\n</table>';
+__p += '\r\n</table>';
 
 }
 return __p
@@ -28,21 +28,21 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<div id="slider">\n';
+__p += '<div id="slider">\r\n';
  _.each(recruiters, function(recruiter) { ;
-__p += '\n\t<div>\n\t\t<img src="' +
+__p += '\r\n\t<div>\r\n\t\t<img src="' +
 ((__t = ( recruiter.url )) == null ? '' : __t) +
 '" alt="' +
 ((__t = ( recruiter.name )) == null ? '' : __t) +
-'">\n\t\t<h4>' +
+'">\r\n\t\t<h4>' +
 ((__t = ( recruiter.name )) == null ? '' : __t) +
 ' : ' +
 ((__t = ( recruiter.title )) == null ? '' : __t) +
 ', ' +
 ((__t = ( recruiter.location )) == null ? '' : __t) +
-'</h4>\n\t</div>\n';
+'</h4>\r\n\t</div>\r\n';
  }); ;
-__p += '\n</div>';
+__p += '\r\n</div>';
 
 }
 return __p
@@ -52,11 +52,11 @@ this["JST"]["src/js/templates/twitterFeed.html"] = function(obj) {
 obj || (obj = {});
 var __t, __p = '', __e = _.escape;
 with (obj) {
-__p += '<h3><span>Twitter Feed</span></h3>\n<a class="twitter-timeline" href="' +
+__p += '<h3><span>Twitter Feed</span></h3>\r\n<a class="twitter-timeline" href="' +
 ((__t = ( url )) == null ? '' : __t) +
 '" data-widget-id="354403948402208768">Tweets by ' +
 ((__t = ( handle )) == null ? '' : __t) +
-'</a>\n<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+'</a>\r\n<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 
 }
 return __p
