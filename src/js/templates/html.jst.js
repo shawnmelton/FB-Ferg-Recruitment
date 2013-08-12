13c26e6ea98be88df3bd3082b41426a342065111
Shawn Melton <shawn.a.melton@gmail.com>
2013-08-12 16:14:00 -0400

    Changes pulled down from FTP

--- src/js/templates/html.jst.js
+++ src/js/templates/html.jst.js
@@ -5,19 +5,19 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<table id="events-list-table">\n\t';
+__p += '<table id="events-list-table">\r\n\t';
  _.each(events, function(event) { ;
-__p += '\n\t<tr' +
+__p += '\r\n\t<tr' +
 ((__t = ( event.class )) == null ? '' : __t) +
-'>\n\t\t<td width="40%">' +
+'>\r\n\t\t<td width="40%">' +
 ((__t = ( event.location )) == null ? '' : __t) +
-'</td>\n\t\t<td width="35%">' +
+'</td>\r\n\t\t<td width="35%">' +
 ((__t = ( event.title )) == null ? '' : __t) +
-'</td>\n\t\t<td width="25%">' +
+'</td>\r\n\t\t<td width="25%">' +
 ((__t = ( event.date )) == null ? '' : __t) +
-'</td>\n\t</tr>\n\t';
+'</td>\r\n\t</tr>\r\n\t';
  }); ;
-__p += '\n</table>';
+__p += '\r\n</table>';
 
 }
 return __p
@@ -30,13 +30,13 @@ function print() { __p += __j.call(arguments, '') }
 with (obj) {
 
  _.each(articles, function(article) { ;
-__p += '\n<article>\n\t<h4>' +
+__p += '\r\n<article>\r\n\t<h4>' +
 ((__t = ( article.title )) == null ? '' : __t) +
-'</h4>\n\t' +
+'</h4>\r\n\t' +
 ((__t = ( article.content )) == null ? '' : __t) +
-'\n\t<a href="' +
+'\r\n\t<a href="' +
 ((__t = ( article.url )) == null ? '' : __t) +
-'" target="_blank" class="upper">Learn More</a>\n</article>\n';
+'" target="_blank" class="upper">Learn More</a>\r\n</article>\r\n';
  }); ;
 
 
@@ -49,21 +49,21 @@ obj || (obj = {});
 var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
 function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '<div id="slider">\n';
+__p += '<div id="slider">\r\n';
  _.each(recruiters, function(recruiter) { ;
-__p += '\n\t<div>\n\t\t<img src="' +
+__p += '\r\n\t<div>\r\n\t\t<img src="' +
 ((__t = ( recruiter.url )) == null ? '' : __t) +
 '" alt="' +
 ((__t = ( recruiter.name )) == null ? '' : __t) +
-'">\n\t\t<h4>' +
+'">\r\n\t\t<h4>' +
 ((__t = ( recruiter.name )) == null ? '' : __t) +
 ' : ' +
 ((__t = ( recruiter.title )) == null ? '' : __t) +
 ', ' +
 ((__t = ( recruiter.location )) == null ? '' : __t) +
-'</h4>\n\t</div>\n';
+'</h4>\r\n\t</div>\r\n';
  }); ;
-__p += '\n</div>';
+__p += '\r\n</div>';
 
 }
 return __p
@@ -77,7 +77,7 @@ __p += '<a class="twitter-timeline" href="' +
 ((__t = ( url )) == null ? '' : __t) +
 '" data-widget-id="354403948402208768">Tweets by ' +
 ((__t = ( handle )) == null ? '' : __t) +
-'</a>\n<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+'</a>\r\n<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 
 }
 return __p
@@ -85,9 +85,18 @@ return __p
 
 this["JST"]["src/js/templates/twitterFeed.html"] = function(obj) {
 obj || (obj = {});
-var __t, __p = '', __e = _.escape;
+var __t, __p = '', __e = _.escape, __j = Array.prototype.join;
+function print() { __p += __j.call(arguments, '') }
 with (obj) {
-__p += '';
+
+ _.each(tweets, function(tweet) { ;
+__p += '\r\n<p>\r\n\t<small>' +
+((__t = ( tweet.date )) == null ? '' : __t) +
+'</small>\r\n\t' +
+((__t = ( tweet.content )) == null ? '' : __t) +
+'\r\n</p>\r\n';
+ }); ;
+
 
 }
 return __p
--- src/js/tools/twitterFeed.js
+++ src/js/tools/twitterFeed.js
@@ -1,29 +1,110 @@
 define(['jquery', 'templates/html.jst', 'libs/sha1', 'libs/codebird'], function($, htmlJST, s1, codebird) {
 	var TwitterFeed = function() {};
 	TwitterFeed.prototype = {
-		feedUrl: "https://twitter.com/FergusonCareers",
-		feedHandle: "@FergusonCareers",
+		feedHandle: "FergusonCareers",
 		el: $("#twitter-feed"),
+		months: new Array(),
+
+		/**
+		 * Prepare tweet content.  Swap links in as well as hash tags.
+		 */
+		prepareContent: function(text, entities) {
+			// Swap in links for urls.
+			for(var index in entities.urls) {
+				text = text.replace(entities.urls[index].url, '<a href="'+ entities.urls[index].expanded_url +'" target="_blank">'+ entities.urls[index].url +'</a>');
+			}
+
+			// Bold hash tags
+			for(var index in entities.hashtags) {
+				text = text.replace("#"+ entities.hashtags[index].text, '<a href="https://twitter.com/search?q=%23'+ entities.hashtags[index].text +'&src=hash" target="_blank">#'+ entities.hashtags[index].text +'</a>');
+			}
+
+			// Media parts
+			for(var index in entities.media) {
+				text = text.replace(entities.media[index].url, '<a href="'+ entities.media[index].expanded_url +'" target="_blank">'+ entities.media[index].display_url +'</a>');
+			}
+
+			// User mentions
+			for(var index in entities.user_mentions) {
+				text = text.replace("@"+ entities.user_mentions[index].screen_name, '<a href="https://twitter.com/'+ entities.user_mentions[index].screen_name +'" target="_blank">@'+ entities.user_mentions[index].screen_name +'</a>');
+			}
+
+			return text;
+		},
+
+		/**
+		 * Prepare tweet date to proper format.
+		 */
+		prepareDate: function(date) {
+			var d = new Date(date).getMonth();
+			if(d >= 0 && d <= 11) {
+				return this.months[d] +" "+ new Date(date).getDate();
+			}
+
+			return "Err "+ new Date(date).getDate();
+		},
+
+		/**
+		 * Strip tweet of unnecessary information.  Convert pertinent data.
+		 */
+		prepareTweet: function(data) {
+			return tweet = {
+				date: this.prepareDate(data.created_at),
+				content: this.prepareContent(data.text, data.entities)
+			};
+		},
 
 		render: function() {
+			this.setMonthsArray();
+
+			var _this = this;
 			var cb = new Codebird;
 			cb.setConsumerKey("wVe1aHQXnSxIRDwjXMjjg", "w1f3tJzkzwwmUFWKN9GAutOG40vSidzc6MVYlP3EbE");
 			cb.setToken("431035831-whH6J5UXuwFfytGLgxGIqYpYOxbdHk1KPRKjIvYN", "P5u91cg0NRqcgV7i6UpZ9IF90AiHnftSXNHWOY2BMN4");
 
+			// Pull latest tweets from FergusonCareers
 			cb.__call(
-			    "users_show",
-			    { screen_name: 'FergusonCareers' },
-			    function (reply) {
-			    	console.log(reply);
-			        // ...
+			    "statuses_userTimeline", {
+			    	screen_name: this.feedHandle,
+			    	count: 3
 			    },
-			    true // this parameter required
-			);
+			    function (response) {
+			    	if(response.httpstatus && response.httpstatus === 200) {
+			    		var tweets = [];
+			    		for(var index in response) {
+			    			if(response[index].created_at) {
+			    				tweets.push(_this.prepareTweet(response[index]));
+			    			}
+			    		}
 
-			/*this.el.html(JST['src/js/templates/twitterFeed.html']({
-				handle: this.feedHandle,
-				url: this.feedUrl
-			}));*/
+			    		if(tweets.length) {
+			    			_this.el.addClass("loaded");
+			    			_this.el.append(JST['src/js/templates/twitterFeed.html']({
+			    				tweets: tweets
+			    			}));
+			    		}
+			    	}
+			    },
+				true
+			);
+		},
+
+		/**
+		 * Set the months array so we can use it to display the properly formatted month.
+		 */
+		setMonthsArray: function() {
+			this.months[0] = "Jan";
+			this.months[1] = "Feb";
+			this.months[2] = "Mar";
+			this.months[3] = "Apr";
+			this.months[4] = "May";
+			this.months[5] = "Jun";
+			this.months[6] = "Jul";
+			this.months[7] = "Aug";
+			this.months[8] = "Sept";
+			this.months[9] = "Oct";
+			this.months[10] = "Nov";
+			this.months[11] = "Dec";
 		}
 	};
 
